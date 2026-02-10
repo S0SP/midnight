47ec88b4e39093d0634211f6c4019ca7f3dece03
s0sp <chourasiasumit51@gmail.com>
2026-02-11 01:13:26 +0530

    Restore @ alias for component imports

--- frontend-vite-react/vite.config.ts
+++ frontend-vite-react/vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig(({ mode }) => ({
   ],
   resolve: {
     alias: {
-      // '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   optimizeDeps: {
